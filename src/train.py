cbdf96a613bb6a3c6f9ad6c20524631eb8f536c4
Max Moser <maxmanski@live.at>
2018-06-10 21:41:27 +0200

    Extend script

--- src/train.py
+++ src/train.py
@@ -10,33 +10,51 @@ import multiprocessing
 import os.path
 
 def vprint(*args, **kwargs):
+    # logging function that is only enabled when the verbose flag is given
     if verbose:
-        print(datetime.datetime.now().isoformat(' ', timespec="seconds"), end=": ")
+        print("%s" % datetime.datetime.now().isoformat(' ', timespec="seconds"), end=": ")
         print(*args, **kwargs)
 
 descr = "Introduction to Natural Language Processing, Project 2"
 epi   = "by Maximilian Moser (01326252) and Wolfgang Weintritt (01327191)"
+
 ap = argparse.ArgumentParser(description=descr, epilog=epi)
 ap.add_argument("--wiki-dump-file", "-w", help="Location of the Wikipedia Dump file", default="dumps/enwiki-*-pages-articles.xml.bz2")
 ap.add_argument("--verbose", "-v", help="Enable verbose output", action="store_true")
 ap.add_argument("--use-cache", "-c", help="Skip the Corpus Creation and use dumped cache file (base directory: out/)")
+ap.add_argument("--cache-output", "-o", help="File name for the cache ouptput (base directory: out/)", default="cache.txt")
+ap.add_argument("--word-vector-output", "-O", help="File name for the saved Word Vector model (base directory: model/)", default="word2vec")
+ap.add_argument("--word-vector-model", "-m", help="File name for the Word Vectors to load instead of train")
 
-args      = ap.parse_args()
-wiki_file = args.wiki_dump_file
-verbose   = args.verbose
-cache     = args.use_cache
+args         = ap.parse_args()
+wiki_file    = args.wiki_dump_file
+verbose      = args.verbose
+cache        = args.use_cache
+cache_output = args.cache_output
+model_input  = args.word_vector_model
+model_output = args.word_vector_output
+
+# make the variables usable
+if not cache.startswith("out/"):
+    cache = "out/" + cache
+
+if not model_output.startswith("model/"):
+    model_output = "model/" + model_output
+
+if model_input is not None and not model_input.startswith("model/"):
+    model_input = "model/" + model_input
 
 if not cache or not os.path.exists(cache):
+    # if no corpus cache was specified, build one
     if not glob.glob(wiki_file):
         print("Could not find Wiki Dump: %s" % wiki_file)
         exit(1)
     else:
         # take the last matching file
         wiki_file = glob.glob(wiki_file)[-1]
-        vprint("Using Wiki Dump: %s" % wiki_file)
 
-    vprint("Creating Corpus from Wiki dump...")
     # parse the wiki dump file into a corpus
+    vprint("Creating Corpus from Wiki Dump '%s'..." % wiki_file)
     wiki = gensim.corpora.wikicorpus.WikiCorpus(wiki_file, lemmatize=False, dictionary={})
     vprint("Done.")
 
@@ -45,12 +63,13 @@ if not cache or not os.path.exists(cache):
     # (LineSentences(file) reads this file and creates a list of lists of words -> iterable of iterables;
     #  the same as wiki.get_texts())
     # https://textminingonline.com/training-word2vec-model-on-english-wikipedia-by-gensim
-    with open("out/cache.txt", "w") as dump_file:
-        vprint("Dumping the Corpus Cache to 'out/cache.txt'...")
+    cache_name = "out/%s" % cache_output
+    with open(cache_name, "w") as dump_file:
+        vprint("Dumping the Corpus Cache to '%s'..." % cache_name)
         # save the created corpus for good measure
         # (don't want the script die on me and lose all progress)
         for text in wiki.get_texts():
-            dump_file.write(b" ".join(text).decode("utf-8") + "\n")
+            dump_file.write(" ".join(text) + "\n")
         vprint("Done.")
 
     # we can use the corpus directly
@@ -58,9 +77,26 @@ if not cache or not os.path.exists(cache):
 
 else:
     # if the cache was specified, use it
-    vprint("Using cached corpus: %s" % cache)
-    inp = LineSentence(cache)
+    vprint("Loading cached corpus: %s" % cache)
+    inp = LineSentence(cache, max_sentence_length=100000)
+    vprint("Done.")
 
-vprint("Training Word2Vec...")
-model = Word2Vec(inp, size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
-vprint("Done.")
+if model_input is None or not os.path.exists(model_input):
+    # if no input model is supplied, we have to train it...
+    vprint("Training Word2Vec...")
+    model = Word2Vec(inp, size=100, window=5, min_count=5, workers=multiprocessing.cpu_count())
+    vprint("Done.")
+else:
+    # else, we just fetch the input model
+    vprint("Loading Word Vector Model '%s'..." % model_input)
+    model = Word2Vec.load(model_input)
+    vprint("Done.")
+
+if model_input is None:
+    # there is no sense in saving a model which we already read from a file...
+    vprint("Saving Word Vector Model to '%s'" % model_output)
+    model.save(model_output)
+    vprint("Done.")
+
+print(model)
+print(model.wv.most_similar("mouse"))
